75e2189f8c150a4012801c79bc4c177bb06a555c
Spergel Balint <spergel.balint@gmail.com>
2020-10-01 19:17:41 +0200

    Complete button Added, Style format
    Complete button and function added,
    Completed Row color

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { Todo } from '../model';
 import { TodoService } from '../model/todo.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDialogComponent } from '../configmdialog/confirmdialog.component';
-import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { MatPaginator, MatSort, MatTableDataSource, Sort } from '@angular/material';
 import { ToDoAddComponent } from '../todoadd/todoadd.component';
 
 
@@ -19,24 +19,31 @@ export class HomeComponent implements OnInit, AfterViewInit  {
   public array: any;
   displayedColumns: string[] = ['name', 'completed', 'operations'];
   dataSource: MatTableDataSource<Todo>;
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string, private todoService: TodoService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.loadTodoList();
-    this.dataSource.paginator = this.paginator;
+    const sortState: Sort = { active: 'completed', direction: 'asc' };
+    this.sort.active = sortState.active;
+    this.sort.direction = sortState.direction;
+    this.sort.sortChange.emit(sortState);
   }
 
   
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
 
   loadTodoList() {
     this.todoService.getTodoList().subscribe(result => {
-      this.dataSource = new MatTableDataSource(result); 
+      this.dataSource = new MatTableDataSource(result);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     },
       error => console.error(error)
     )
@@ -64,6 +71,16 @@ export class HomeComponent implements OnInit, AfterViewInit  {
           error => console.error(error)
         );
       }
+    });
+  }
+
+  done(elem: Todo) {
+    const updateTodo = new Todo();
+    updateTodo.name = elem.name;
+    updateTodo.id = elem.id;
+    updateTodo.completed = true;
+    this.todoService.update(elem.id, updateTodo).subscribe(result => {
+      this.loadTodoList();
     });
   }
 
